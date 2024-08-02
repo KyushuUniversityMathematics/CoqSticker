e3a673c1aa2be6ebe8e87d9ac160418aadd729e0
gingahayakawa <galaxy.47ag@gmail.com>
2024-08-02 19:48:31 +0900

    20240802

--- sticker/myAutomaton.v
+++ sticker/myAutomaton.v
@@ -7,25 +7,33 @@ Structure automaton{state symbol:finType}:= Automaton {
   delta : state -> symbol -> state;
   final : {set state}
 }.
+
+(*記号を複数個まとめて読み込む関数*)
 Fixpoint dstar{state symbol:finType}(delta:state->symbol->state)
   (q:state)(str:seq symbol):state :=
 match str with
 |nil => q
 |h::str' => dstar delta (delta q h) str'
 end.
+
+(*受理判定の関数*)
 Definition accept{state symbol:finType}(M:@automaton state symbol)
   (str:seq symbol):bool := dstar (delta M) (init M) str\in final M.
+
+(*与えられた文字列のうち、acceptされるものだけを残す*)
 Definition accepts{state symbol:finType}(M:@automaton state symbol)
   (l:seq (seq symbol)):seq (seq symbol):=
 [seq str<-l|accept M str].
 
+(*文字の読み込みを二段階に分割*)
 Lemma dstarLemma {state symbol : finType}(delta:state->symbol->state)(q:state)
 (s t:seq symbol):dstar delta q (s++t) = dstar delta (dstar delta q s) t.
 Proof. move:q;by elim:s;[|move=>a s' H;simpl]. Qed.
 
 
 
-
+(*以下はasciiやboolに有限型(finType)をつけて具体的なオートマトンを構成している。
+証明とは無関係*)
 Open Scope nat_scope.
 
 Definition eq_string a b :=
--- sticker/mySticker.v
+++ sticker/mySticker.v
@@ -1,9 +1,12 @@
 From mathcomp Require Import all_ssreflect.
 Require Import Ascii String.
 
+(*ドミノの定義　nullはエラー型で、通常のドミノは二本のリストと2つのnatで表現*)
 Inductive domino{symbol:finType}:=
 |null : domino
 |Domino :seq symbol -> seq symbol -> nat -> nat -> domino.
+
+(*ドミノにeqTypeを付ける。ドミノに==の関数を適用できるように*)
 Definition eqb_domino{s:finType}(d1 d2:@domino s):bool :=
 match d1,d2 with
 |null,null => true
@@ -37,6 +40,7 @@ Canonical domino_eqMixin{symbol:finType} := EqMixin (@eq_dominoP symbol).
 Canonical domino_eqType{symbol:finType} := 
   Eval hnf in EqType _ (@domino_eqMixin symbol).
 
+(*ドミノの粘着演算　粘着できない組み合わせはnullとなる*)
 Definition mu{symbol:finType}(d1 d2:@domino symbol):@domino symbol:=
 match d1,d2 with
 |Domino nil _ _ _ ,Domino nil _ _ _ => null
@@ -54,8 +58,13 @@ match d1,d2 with
     then Domino(l1++l2)(r1++r2)x1 y1 else null
 |_,_ => null
 end.
+
+(*ドミノに対し、ドミノ対を左右から挟み込むように粘着させる*)
 Definition mu'{s:finType}(a:@domino s)(d:@domino s*@domino s):@domino s:=
 let (d1,d2) := d in mu (mu d1 a) d2.
+
+(*ドミノの両端が揃っていること、
+上側鎖と下側鎖が相補的関係(rho)で対応付けられているかのチェック*)
 Definition wk{s:finType}(rh:seq (s*s))(d:@domino s):bool :=
 match d with
 |Domino l r 0 0 =>
@@ -65,11 +74,15 @@ match d with
     false
 |_ => false
 end.
+
+(*スティッカー本体 これ自体はデータの塊で、計算機能は別で定義する*)
 Structure sticker{symbol:finType}:= Sticker{
   rho : seq (symbol*symbol);
   start : seq (@domino symbol);
   extend : seq (@domino symbol*@domino symbol)
 }.
+
+(*ドミノをn回作用させた際に作られ得るドミノの一覧*)
 Fixpoint ss_generate{symbol:finType}(n:nat)(stk:@sticker symbol):seq domino:=
 match n with
 |0 => start stk
@@ -77,6 +90,7 @@ match n with
   A'++[seq mu' a d|a<-A',d <- (extend stk)]
 end.
 
+(*原始的な計算(粘着末端が消えたら計算終了)に限ったバージョン*)
 Fixpoint ss_generate_prime{s:finType}(n:nat)(stk:@sticker s):seq domino:=
 match n with
 |0 => start stk
@@ -87,12 +101,17 @@ match n with
   A_wk++[seq mu' a d|a<-A_nwk,d <- (extend stk)]
 end.
 
+(*ドミノからのコーディング　上側鎖の文字列情報を読み取る*)
 Definition ss_language_f{s:finType}(d:@domino s):seq s :=
 match d with
 |Domino l _ _ _ => l
 |_ => nil
 end.
+
+(*stickerがn回の作用で生成できる言語一覧*)
 Definition ss_language{s:finType}(n:nat)(stk:@sticker s):seq (seq s) :=
 [seq ss_language_f d | d <- [seq d <- ss_generate n stk|wk (rho stk) d]].
+
+(*原始的な計算ver.*)
 Definition ss_language_prime{s:finType}(n:nat)(stk:@sticker s):seq (seq s) :=
 [seq ss_language_f d | d <- [seq d <- ss_generate_prime n stk|wk (rho stk) d]].
