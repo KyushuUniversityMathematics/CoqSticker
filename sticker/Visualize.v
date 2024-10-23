5f92268d41d37f7af23c855c1807f6670c547331
gingahayakawa <galaxy.47ag@gmail.com>
2024-10-23 14:02:10 +0900

    20241023

--- sticker/Visualize.v
+++ sticker/Visualize.v
@@ -1,3 +1,4 @@
+
 From mathcomp Require Import all_ssreflect.
 Require Import AutomatonModule StickerModule String Ascii Ex.
 
@@ -10,14 +11,23 @@ match s with
 |h::s' => String(f h)(SymbolString f s')
 end.
 
+Definition EndVisualize{symbol:finType}(f:symbol->ascii)(x:@stickyend symbol):=
+match is_upper x with
+|true => "\begin{pmatrix}"++SymbolString f(end_str x)++"\\\lambda\end{pmatrix}"
+|false => "\begin{pmatrix}\lambda\\"++SymbolString f(end_str x)++"\end{pmatrix}"
+end.
+Definition WkVisualize{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)(x:@wk symbol rho):=
+"\begin{pmatrix}"++SymbolString f(unzip1(str x))++"\\"++SymbolString f(unzip2(str x))++"\end{pmatrix}".
+
 Definition DominoVisualize{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)
 (x:@domino symbol rho):string :=
 match x with
 |null => "\begin{pmatrix}\lambda\\\lambda\end{pmatrix}"
-|Simplex (Se true w _) => "\begin{pmatrix}"++SymbolString f w++"\\\lambda\end{pmatrix}"
-|Simplex (Se false w _) => "\begin{pmatrix}\lambda\\"++SymbolString f w++"\end{pmatrix}"
-|WK (Wk w _ _) => "\begin{pmatrix}"++SymbolString f(unzip1 w)++"\\"++SymbolString f(unzip2 w)++"\end{pmatrix}"
-|_ => ""
+|Simplex s => EndVisualize f s
+|WK w => WkVisualize f w
+|L s w => EndVisualize f s ++ WkVisualize f w
+|R w s => WkVisualize f w++ EndVisualize f s
+|LR l w r => EndVisualize f l ++ WkVisualize f w ++ EndVisualize f r
 end.
 
 Fixpoint DominoVisualize'{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)
@@ -26,10 +36,27 @@ match s with
 |nil => ""
 |x::s' => (DominoVisualize f x)++"//"++(DominoVisualize' f s')
 end.
+Definition DominoPairVisualize{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)(x:(@domino symbol rho)*(@domino symbol rho)):=
+let (l,r):= x in "\left("++DominoVisualize f l ++ DominoVisualize f r++"\right)".
+Fixpoint DominoPairVisualize'{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)
+(s:seq((@domino symbol rho)*(@domino symbol rho))):string:=
+match s with
+|nil => ""
+|x::s' => (DominoPairVisualize f x)++"//"++(DominoPairVisualize' f s')
+end.
 Definition abAscii(x:ab):ascii:=match x with|a=>"A"|b=>"B"end.
 
-Definition StickerVisualize{symbol:finType}{rho:Rho symbol}
-(s:@sticker symbol rho):String :=
 
+Definition StickerVisualize{symbol:finType}{rho:Rho symbol}(f:symbol->ascii)
+(s:@sticker symbol rho):string :=
+DominoVisualize' f (start s) ++"\\\\"++DominoPairVisualize' f(extend s).
+
+
+Compute (Aut_to_Stk mp).
+Compute mp.
+Compute StickerVisualize abAscii(Aut_to_Stk mp).
+Compute DominoVisualize abAscii(WK(mkwkzip a[::b;a;a;b])).
+Definition s:sticker := Sticker ab_finType (zip (enum ab_finType)(enum ab_finType)) [::(WK(mkwkzip a[::b;a;a;b]))] [::((WK(mkwkzip a[::b;a;a;b])),(WK(mkwkzip a[::b;a;a;b])))] erefl.
+Compute StickerVisualize abAscii s.
 
 
